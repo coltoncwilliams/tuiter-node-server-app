76b575d29cb47569239582bbeb8825fe54bae5b2
cole242000@gmail.com <cole242000@gmail.com>
2022-11-19 20:14:06 -0500

    updating starting tweet

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -6,6 +6,9 @@ const createTuit = (req, res) => {
   newTuit._id = (new Date()).getTime()+'';
   newTuit.likes = 0;
   newTuit.liked = false;
+  newTuit.dislikes = 0;
+  newTuit.disliked = false;
+  newTuit.image = 'nasa.png'
   tuits.push(newTuit);
   res.json(newTuit);
 }
