9b8882e657af29ec030a41ebed56a6bfc68d2725
cole242000@gmail.com <cole242000@gmail.com>
2022-11-26 22:50:37 -0500

    updating create tuit

--- tuits/tuits-schema.js
+++ tuits/tuits-schema.js
@@ -1,7 +1,15 @@
 import mongoose from 'mongoose';
 const schema = mongoose.Schema({
+    userName: String,
+    handle: String,
+    image: String,
     tuit: String,
     likes: Number,
+    dislikes: Number,
     liked: Boolean,
+    disliked: Boolean,
+    time: String,
+    replies: Number,
+    retuits: Number
 }, {collection: 'tuits'});
 export default schema;
