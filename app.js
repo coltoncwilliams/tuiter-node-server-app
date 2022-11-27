cba124177927cb367a70152d856a7dcb3a7d46a5
cole242000@gmail.com <cole242000@gmail.com>
2022-11-26 22:16:18 -0500

    adding database

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ import cors from 'cors'
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js"
-const app = express()
-app.use(express.json())
-app.use(cors())
-TuitsController(app)
-HelloController(app)
-UserController(app)
-app.listen(process.env.PORT || 4000)
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING).then(r => {
+    const app = express()
+    app.use(express.json())
+    app.use(cors())
+    TuitsController(app)
+    HelloController(app)
+    UserController(app)
+    app.listen(process.env.PORT || 4000)
+});
